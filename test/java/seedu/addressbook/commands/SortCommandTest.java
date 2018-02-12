87eb63105d71108b5b41cf904148eacf631878de
Shannon Wong <shanwpf@gmail.com>
2018-02-12 17:03:20 +0800

    Create and update tests

---
+++ test/java/seedu/addressbook/commands/SortCommandTest.java
@@ -0,0 +1,42 @@
+package seedu.addressbook.commands;
+
+import org.junit.Test;
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.UniquePersonList;
+import seedu.addressbook.util.TypicalPersons;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+public class SortCommandTest {
+
+    private AddressBook addressBook;
+    private final TypicalPersons td = new TypicalPersons();
+
+    @Test
+    public void execute() throws IllegalValueException {
+        // Before sorting: bill, amy, dan, candy
+        // Expected after sorting: amy, bill, dan, candy
+        assertSortCommandBehavior(new UniquePersonList(td.amy, td.bill, td.candy, td.dan));
+    }
+
+    /**
+     * Executes the sort command for the given address book and verifies
+     * the order of persons in result matches persons in the expectedPersonList exactly.
+     */
+    private void assertSortCommandBehavior(UniquePersonList expectedPersonList) {
+        SortCommand command = createSortCommand();
+        command.execute();
+        assertEquals(addressBook.getAllPersons(), expectedPersonList);
+    }
+
+    private SortCommand createSortCommand() {
+        SortCommand command = new SortCommand();
+        this.addressBook = new TypicalPersons().getTypicalAddressBook();
+        command.setData(addressBook, Collections.emptyList());
+        return command;
+    }
+
+}
--- test/java/seedu/addressbook/util/TypicalPersons.java
+++ test/java/seedu/addressbook/util/TypicalPersons.java
@@ -19,14 +19,14 @@ public class TypicalPersons {
 
     public TypicalPersons() {
         try {
-            amy = new Person(new Name("Amy Buck"), new Phone("91119111", false), new Email("ab@gmail.com", false),
-                    new Address("1 Clementi Road", false), new UniqueTagList());
             bill = new Person(new Name("Bill Clint"), new Phone("92229222", false), new Email("bc@gmail.com", false),
                     new Address("2 Clementi Road", true), new UniqueTagList());
-            candy = new Person(new Name("Candy Destiny"), new Phone("93339333", true),
-                    new Email("cd@gmail.com", false), new Address("3 Clementi Road", true), new UniqueTagList());
+            amy = new Person(new Name("Amy Buck"), new Phone("91119111", false), new Email("ab@gmail.com", false),
+                    new Address("1 Clementi Road", false), new UniqueTagList());
             dan = new Person(new Name("Dan Smith"), new Phone("1234556", true), new Email("ss@tt.com", true),
                     new Address("NUS", true), new UniqueTagList(new Tag("Test")));
+            candy = new Person(new Name("Candy Destiny"), new Phone("93339333", true),
+                    new Email("cd@gmail.com", false), new Address("3 Clementi Road", true), new UniqueTagList());
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
